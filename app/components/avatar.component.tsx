147f0c1daa59e3521146f2fe6c500a85727744f6
TecOp <rodrigo.froes@futcorp.com.br>
2024-10-22 17:58:08 -0300

    Feat: Button para navigator

--- app/components/avatar.component.tsx
+++ app/components/avatar.component.tsx
@@ -1,12 +1,16 @@
+import { TouchableOpacity } from 'react-native';
 import UserAvatar from 'react-native-user-avatar';
 
 interface AvatarProps {
     size: number;
     name: string;
+    onPress?: () => void;
 }
 
-export default function Avatar({size, name}: AvatarProps){
+export default function Avatar({size, name, onPress}: AvatarProps){
     return(
-        <UserAvatar size={size} name={name} />
+        <TouchableOpacity onPress={onPress}> 
+            <UserAvatar size={size} name={name} />
+        </TouchableOpacity>
     );
 }
